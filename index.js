d96d696ed3206c1289e80b5808f7d696cde4eb4e
Yossapong <yossapong.ra@ofm.co.th>
2019-08-29 14:27:15 +0700

    Update

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ app.post('/webhook', (req, res) => {
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+      console.log(webhook_event[0]['message']['text']);
+     console.log(webhook_event[0]['id']);
     });
 
     // Returns a '200 OK' response to all requests
