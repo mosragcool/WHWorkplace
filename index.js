ebe5de7f3dac4365f0014ed2d3015e5d773d69f4
Yossapong <yossapong.ra@ofm.co.th>
2019-08-29 15:37:26 +0700

    Update

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ app.post('/webhook', (req, res) => {
       let webhook_event = entry.messaging[0];
       let sender_psid = webhook_event.sender.id;
       if (webhook_event.message) {
-     
-        handleMessage(sender_psid, webhook_event.message);        
+        console.log(webhook_event);
+      //  handleMessage(sender_psid, webhook_event.message);        
       }
       else
       {
