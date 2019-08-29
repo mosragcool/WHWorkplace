a09ba435624a286e55739680b4a0a22785333450
Yossapong <yossapong.ra@ofm.co.th>
2019-08-29 21:45:43 +0700

    Update

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ https.request(options, function(res) {
   console.log('STATUS: ' + res.statusCode);
   console.log('HEADERS: ' + JSON.stringify(res.headers));
   res.setEncoding('utf8');
-  res.headers(header);
+  res.headers("Content-Type","application/json");
   res.on('data', function (chunk) {
     console.log('BODY: ' + chunk);
   });
