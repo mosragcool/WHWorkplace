e53b88b3635456109c9505227328d5d38598af90
Yossapong <yossapong.ra@ofm.co.th>
2019-08-30 16:22:14 +0700

    Update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ bodyParser = require('body-parser'),
 app = express().use(bodyParser.json()); // creates express http server
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 6578, () => console.log('webhook is listening'));
+//app.listen(process.env.PORT || 6578, () => console.log('webhook is listening'));
 
 
 // Creates the endpoint for our webhook
@@ -14,9 +14,9 @@ app.listen(process.env.PORT || 6578, () => console.log('webhook is listening'));
 app.post('/webhook', (req, res) => {  
  
   let body = req.body;
-
+  console.log(body.object);
   if (body.object === 'page') {
-
+  
 
     body.entry.forEach(function(entry) {
 
@@ -154,6 +154,6 @@ app.get('/webhook', (req, res) => {
 
 
 
-//app.listen(process.env.PORT || 1234)
+app.listen(process.env.PORT || 1234);
 //app.listen(1234);
 //https.createServer(app).listen(1234);
