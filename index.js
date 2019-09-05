f27ada79323b6b4951cb023c5a018bef0db65b92
Yossapong <yossapong.ra@ofm.co.th>
2019-09-05 17:15:14 +0700

    Update

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ function ProcessMessage(sender_psid,message)
       });
 
 
-      if(command[0] === 'S')
+      if(command[0].toUpperCase() === 'S')
       {
         var options = {
           host: '10.17.1.32',
