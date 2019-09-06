4f6cd84136684b16a8779e1948b9ec4bec037d9c
Yossapong <yossapong.ra@ofm.co.th>
2019-09-06 11:08:23 +0700

    Update

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post('/webhook', (req, res) => {
  
     if(botID == recipient.id)
   {
-    console.log(body.entry[0].changes[0].value.to);
+    console.log(body.entry[0].changes[0].value);
 
 
 if(count > 1 )//& recipient.name.)
