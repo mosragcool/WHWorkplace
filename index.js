035bf9ba608e296bc4132136a9c7823f99d7a8a6
Yossapong <yossapong.ra@ofm.co.th>
2019-09-06 10:59:29 +0700

    Update

--- index.js
+++ index.js
@@ -24,14 +24,14 @@ app.post('/webhook', (req, res) => {
  body.entry.forEach(function(entry) {
 
   //console.log(entry);
-  console.log(body.entry[0].changes);
+  //console.log(body.entry[0].changes);
   //console.log(entry.changes[0].value.from);
   console.log(body.entry[0].changes[0].value.to);
 
   var sender_psid = entry.changes[0].value.from.id;
   var message = entry.changes[0].value.message;
  var count = entry.changes[0].value.to.data.length;
- console.log("จำนวนคนส่ง "+count);
+ //console.log("จำนวนคนส่ง "+count);
 
   entry.changes[0].value.to.data.forEach(function(recipient){
 
